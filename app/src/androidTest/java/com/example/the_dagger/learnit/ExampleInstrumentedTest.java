b2e2934a448414bab80b26e4402445b22ad70121
Anurag Kumar <akanuragkumar712@gmail.com>
2017-04-07 15:06:24 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/the_dagger/learnit/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/the_dagger/learnit/ExampleInstrumentedTest.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
+import org.junit.Test; 
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+ 
         assertEquals("com.example.the_dagger.learnit", appContext.getPackageName());
     }
 }
